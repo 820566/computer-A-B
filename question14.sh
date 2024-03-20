126563242d207c0d62c84a877aadea457cb6378a
820566 <125930314+820566@users.noreply.github.com>
2024-03-20 17:45:52 +1030

    Update question14.sh

--- question14.sh
+++ question14.sh
@@ -1,6 +1,10 @@
 git clone  https://github.com/ian-knight-uofa/git-practice-04.git
 git remote rm origin
 git remote add https://github.com/ian-knight-uofa/git-practice-04.git
+cd git-practice-04
 git pull origin main
 git fetch origin main
 git switch update1
+git checkout update1
+git pull
+git commit -m "update1"

727f26e8abd0cb07a9d8e24a501dbdd322ae5e1e
820566 <125930314+820566@users.noreply.github.com>
2024-03-20 17:13:03 +1030

    Create question18.sh

---
+++ question18.sh
@@ -0,0 +1,19 @@
+git clone  https://github.com/ian-knight-uofa/git-practice-04.git
+git remote rm origin
+git remote https://github.com/ian-knight-uofa/git-practice-04.git
+cd git practice-04.git
+git pull origin main
+git checkout ready1
+git pull
+git checkout ready2
+git pull
+git checkout ready3
+git pull
+git checkout update1
+git pull
+git checkout update2
+git pull
+git commit -m "branch"
+git merge -m "ready1" ready1
+git merge -m "ready2" ready2
+git merge -m "ready3" ready3

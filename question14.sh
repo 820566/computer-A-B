7ef85fcd89819f1053b769b9d27b34e2252f92d3
820566 <125930314+820566@users.noreply.github.com>
2024-03-20 17:11:12 +1030

    Create question14.sh

---
+++ question14.sh
@@ -0,0 +1,6 @@
+git clone  https://github.com/ian-knight-uofa/git-practice-04.git
+git remote rm origin
+git remote add https://github.com/ian-knight-uofa/git-practice-04.git
+git pull origin main
+git fetch origin main
+git switch update1

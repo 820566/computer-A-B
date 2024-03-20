65ecc256eb48bb55341971ed7d1aaefdcdb14cf4
820566 <125930314+820566@users.noreply.github.com>
2024-03-20 17:12:07 +1030

    Create question16.sh

---
+++ question16.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/ian-knight-uofa/git-practice-02.git
+git remote rm origin
+git remote https://github.com/ian-knight-uofa/git-practice-02.git
+cd git practice-02.git
+git pull origin main
+git fetch origin main
+git checkout branch3
+git pull
+git checkout branch2
+git pull
+git commit -m"done"
+git merge branch3
+git branch -d branch3

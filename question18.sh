7bc5f6e010a757d9839fda2e1e548b7331213477
820566 <125930314+820566@users.noreply.github.com>
2024-03-22 01:28:43 +1030

    Update question18.sh

--- question18.sh
+++ question18.sh
@@ -2,18 +2,22 @@ git clone  https://github.com/ian-knight-uofa/git-practice-04.git
 git remote rm origin
 git remote origin https://github.com/ian-knight-uofa/git-practice-04.git
 cd git-practice-04
-git pull origin main
+git fetch origin 
+git branch -a
 git checkout ready1
-git pull
 git checkout ready2
-git pull
 git checkout ready3
-git pull
-git checkout update1
-git pull
-git checkout update2
-git pull
+
+
+git checkout main
 git commit -m "branch"
 git merge -m "ready1" ready1
 git merge -m "ready2" ready2
 git merge -m "ready3" ready3
+git branch -d ready1
+git branch -d ready2
+git branch -d ready3
+git checkout update1
+git merge main
+git checkout update2
+git merge main

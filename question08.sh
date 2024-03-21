e70154411fafbc43652de8fe558445066bf15554
820566 <125930314+820566@users.noreply.github.com>
2024-03-22 00:52:03 +1030

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -1,4 +1,8 @@
+#mkdir dir2
+#mv *.txt dir2
+#git add dir2/*.txt
+#git commit -m "qusetion8"
 mkdir dir2
 mv *.txt dir2
-git add dir2/*.txt
-git commit -m "qusetion8"
+git add *
+git commit -m"mv txt"

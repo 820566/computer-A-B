8f3450d2a5b96c5f9ee1bcf46db756dfe17529fe
820566 <125930314+820566@users.noreply.github.com>
2024-03-21 10:35:23 +1030

    Update question13.sh

--- question13.sh
+++ question13.sh
@@ -4,5 +4,5 @@ touch file13.txt
 git add file13.txt
 git commit -m "question13"
 git remote rm origin
-git remote add origin 
+git remote add origin https://github.com/820566/computer-A-B.git
 git push origin submission_repository

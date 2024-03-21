35b50d0229498588e0faa6745ef1f55554270945
820566 <125930314+820566@users.noreply.github.com>
2024-03-21 11:02:21 +1030

    Update question15.sh

--- question15.sh
+++ question15.sh
@@ -2,8 +2,8 @@ git clone https://github.com/ian-knight-uofa/git-practice-01.git
 git remote rm origin
 git remote add origin https://github.com/ian-knight-uofa/git-practice-01.git
 cd git practice-01
-git fetch origin
 git checkout main
+git fetch origin
 git add dir1/file3
 git add file1
 git commit -m "-ssss"

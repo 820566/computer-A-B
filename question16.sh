5256832f86a614991ab35b9f3db56411588dade7
820566 <125930314+820566@users.noreply.github.com>
2024-03-21 11:13:41 +1030

    Update question16.sh

--- question16.sh
+++ question16.sh
@@ -2,12 +2,8 @@ git clone https://github.com/ian-knight-uofa/git-practice-02.git
 git remote rm origin
 git remote add origin https://github.com/ian-knight-uofa/git-practice-02.git
 cd git-practice-02
-git pull origin main
-git fetch origin main
-git checkout branch3
-git pull
+git fetch origin 
 git checkout branch2
-git pull
-git commit -m"done"
-git merge branch3
+git merge -m "msdf" origin/branch3
+
 git branch -d branch3
